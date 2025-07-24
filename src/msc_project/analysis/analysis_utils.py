b25dc1641348688142a7f91a3e56ed708c9ebce4
bella <annabel.jakob@hotmail.de>
2025-07-24 12:23:21 +0100

    fix f-string again

--- src/msc_project/analysis/analysis_utils.py
+++ src/msc_project/analysis/analysis_utils.py
@@ -155,7 +155,8 @@ def plot_distribution(data, mean, std, kurt, param_type, ax, model_name = None, 
     ax.axvline(mean, color="r", linestyle="dashed", linewidth=2, label=f"Mean: {mean:.4f}")
 
     if model_name and category_name:
-        ax.set_title(f'Distribution of "{category_name}" {"Weights" if param_type == "Weight" else "Biases"} in {model_name}', fontsize=16)
+        title = f'Distribution of "{category_name}" {"Weights" if param_type == "Weight" else "Biases"} in {model_name}'
+        ax.set_title(title, fontsize=16)
     else:
         ax.set_title(f'Distribution of {"Weights" if param_type == "Weight" else "Biases"}', fontsize=16)
     ax.set_xlabel(f"{param_type} Value", fontsize=12)
