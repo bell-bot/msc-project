4ffe09c97fb87ce32fae974f9007685fd7e63d05
bella <annabel.jakob@hotmail.de>
2025-07-26 17:32:17 +0100

    adjust fitness weight distribution

--- experiments/experiment_genetic_algorithm.py
+++ experiments/experiment_genetic_algorithm.py
@@ -33,14 +33,16 @@ def fitness_func(ga_instance, solution, solution_idx):
         if not torch.allclose(predicted_output, output_tensor, atol=1e-10):
             return 0.0
         
-        fitness = 100.0
+        fitness = 10.0
+        
         # Objective 2: Robustness to noise
         noise = np.random.normal(0, 0.1, solution.shape)
         noisy_solution = solution + noise
         pygad.torchga.model_weights_as_dict(model=mlp_template, weights_vector=noisy_solution)            
         noisy_output = mlp_template(input_tensor)
         if torch.allclose(noisy_output, output_tensor, atol=1e-4):
-            fitness += 50.0
+            fitness += 5.0
+
         # Objective 3: Obscurity
         fitness += evaluate_normal_distribution(solution)
         end_time = time.time()
