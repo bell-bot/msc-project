823652018e365eac1201e3f233458c0707202700
Bella <annabel.jakob@chch.ox.ac.uk>
2025-09-29 15:52:13 +0100

    Implement sampling from target model

---
+++ src/msc_project/utils/sampling.py
@@ -0,0 +1,21 @@
+from typing import Literal
+
+import torch
+
+def sample_from_distribution(target: torch.Tensor, num_samples: int, sign: Literal["positive", "negative"] = "positive") -> torch.Tensor:
+
+    if sign == "positive":
+        target = target[target > 0]
+    elif sign == "negative":
+        target = target[target < 0]
+
+    num_available = target.numel()
+
+    if num_available == 0:
+        raise ValueError(f"No {sign} values available for sampling.")
+    
+    if num_available >= num_samples:
+        indices = torch.randperm(num_available, device=target.device)[:num_samples]
+    else:
+        indices = torch.randint(num_available, (num_samples,), device=target.device)
+    return target[indices]
