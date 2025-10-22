b50617ed69b135857e0c52a211e87b617efa236d
Bella <annabel.jakob@chch.ox.ac.uk>
2025-10-22 13:06:48 +0100

    Implement method to generate random Bits

--- src/msc_project/utils/run_utils.py
+++ src/msc_project/utils/run_utils.py
@@ -1,11 +1,17 @@
-import random
 import string
-import numpy as np
-from numpy.random import RandomState
 import torch
+
+from circuits.utils.format import Bits
 
 
 def get_random_alphanum_string(num_chars=16):
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
     indices = torch.randint(low=0, high=len(chars), size=(num_chars,))
     return "".join(chars[i] for i in indices)
+
+
+def get_random_bits(num_bits=16) -> Bits:
+    chars = "01"
+    indices = torch.randint(low=0, high=len(chars), size=(num_bits,))
+    bitstr = "".join(chars[i] for i in indices)
+    return Bits.from_str(bitstr)
